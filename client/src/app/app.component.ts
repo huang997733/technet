51a7c5d2af50c01ddd51c2304a6642399fafeb55
huang997733 <williamhzy99@gmail.com>
2024-03-21 17:22:30 +1100

    retrieve data from API

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { HttpClient } from '@angular/common/http';
+import { NgFor } from '@angular/common';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, NavBarComponent, NgFor]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'TechNet';
+  products: Product[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({
+      next: response => this.products = response.data,
+      error: error => console.error(error),
+      complete: () =>  {
+        console.log('completed');
+        console.log('extra statement');
+      }
+    })
+  }
 }
