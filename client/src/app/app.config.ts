7f6228e6a74c4396332ef3cb63aaf7a62ad99bc8
huang997733 <williamhzy99@gmail.com>
2024-03-27 21:40:20 +1100

    add error handling (interceptors)

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -2,9 +2,25 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
+import { provideToastr } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),
-  provideHttpClient(withFetch())]
+  providers: [
+    provideAnimations(),
+    provideToastr({
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+
+    }),
+    provideRouter(routes),
+    provideHttpClient(withFetch()), 
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide:HTTP_INTERCEPTORS,
+      useClass: errorInterceptor,
+      multi: true
+    }]
 };
