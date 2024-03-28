b1961c59ba0eea01008d80eb1fc526949f1c296c
huang997733 <williamhzy99@gmail.com>
2024-03-28 15:28:59 +1100

    setup redis to store basket

---
+++ Core/Interfaces/IBasketRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Core;
+
+public interface IBasketRepository
+{
+    Task<CustomerBasket> GetBasketAsync(string basketId);
+    Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket);
+    Task<bool> DeleteBasketAsync(string basketId);
+}
